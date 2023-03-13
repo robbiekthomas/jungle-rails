7b11f6a0c287718a869c39f7eb4d0330fc4e4b0d
Robbie Thomas <robbiekt@gmail.com>
2023-03-13 11:05:27 -0700

    create user auth and validation and rspec tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :password, presence: true, confirmation: true, length: { minimum: 8 }
+  validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
 end
