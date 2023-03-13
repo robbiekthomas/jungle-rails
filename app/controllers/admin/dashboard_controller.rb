37fbc674c803102552bf950dd08217a0a67b81e5
Robbie Thomas <robbiekt@gmail.com>
2023-03-13 12:50:34 -0700

    add cypress testing for home page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD']
   def show
     @products_count = Product.count
     @categories_count = Category.count
